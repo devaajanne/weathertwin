dae45c25ffba915015c1c2ceeda24cd5b5f646a2
devaajanne <devaajanne@gmail.com>
2024-12-19 13:59:14 +0200

    Added GET REST API call method to get longitude and latitude of a given city

--- src/main/java/app/weathertwin/controller/WeathertwinController.java
+++ src/main/java/app/weathertwin/controller/WeathertwinController.java
@@ -1,8 +1,26 @@
 package app.weathertwin.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import app.weathertwin.service.HttpService;
+
+@RestController
+@RequestMapping("api/weather")
 public class WeathertwinController {
+
+    @GetMapping("/{city}")
+    public HashMap<String, Double> findLatAndLon(@PathVariable String city) {
+        HashMap<String, Double> latAndLonMap = new HashMap<String, Double>();
+
+        latAndLonMap = HttpService.fetchLatAndLon(city);
+
+        return latAndLonMap;
+    }
 
 }
