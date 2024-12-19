e98d2e1cb0c1a76eb935f40b5c00a11530afc86b
devaajanne <devaajanne@gmail.com>
2024-12-19 14:40:42 +0200

    Added property source to Spring boot application

--- src/main/java/app/weathertwin/WeathertwinApplication.java
+++ src/main/java/app/weathertwin/WeathertwinApplication.java
@@ -2,12 +2,15 @@ package app.weathertwin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-dev.properties")
 public class WeathertwinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeathertwinApplication.class, args);
+		System.out.println("Weather Twin is running");
 	}
 
 }
