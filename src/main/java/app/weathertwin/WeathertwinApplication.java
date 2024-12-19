19ec4293dfa03d72f738138f27de217a6a54af7d
devaajanne <devaajanne@gmail.com>
2024-12-19 11:13:22 +0200

    Initial commit

---
+++ src/main/java/app/weathertwin/WeathertwinApplication.java
@@ -0,0 +1,13 @@
+package app.weathertwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathertwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathertwinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/weathertwin/WeathertwinApplicationTests.java
@@ -0,0 +1,13 @@
+package app.weathertwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathertwinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
