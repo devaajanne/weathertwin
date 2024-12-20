bc4c9fd81870df4440bee76ea179adb22672e7f3
devaajanne <devaajanne@gmail.com>
2024-12-20 17:48:28 +0200

    Updated controller to return a hashmap of input and similar location, fixed imports

--- src/main/java/app/weathertwin/controller/WeathertwinController.java
+++ src/main/java/app/weathertwin/controller/WeathertwinController.java
@@ -1,16 +1,13 @@
 package app.weathertwin.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.HashMap;
 
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import app.weathertwin.service.HttpService;
 
@@ -25,13 +22,17 @@ public class WeathertwinController {
     }
 
     @GetMapping("/{city}")
-    public JsonNode getCityWeatherData(@PathVariable String city) {
-        HashMap<String, Double> latAndLonMap = new HashMap<String, Double>();
-        JsonNode cityWeatherData = JsonNodeFactory.instance.objectNode();
+    public HashMap<String, JsonNode> getCityWeatherData(@PathVariable String city) {
+        HashMap<String, JsonNode> returnedMap = new HashMap<String, JsonNode>();
 
-        latAndLonMap = httpService.fetchLatAndLon(city);
-        cityWeatherData = httpService.fetchWeatherData(latAndLonMap.get("lat"), latAndLonMap.get("lon"));
+        HashMap<String, Double> latAndLonMap = httpService.fetchLatAndLon(city);
+        JsonNode cityWeatherData = httpService.fetchWeatherData(latAndLonMap.get("lat"), latAndLonMap.get("lon"));
 
-        return cityWeatherData;
+        // TODO: implement logic/call method to find a city with similar weather
+
+        returnedMap.put("inputLocation", cityWeatherData);
+        returnedMap.put("similarLocation", null);
+
+        return returnedMap;
     }
 }
