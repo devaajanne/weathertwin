eef006bcbeaddaa6a59a16a2c6c963090c9c8a74
devaajanne <devaajanne@gmail.com>
2024-12-21 15:50:55 +0200

    Updated WeatherTwin application

--- src/main/java/app/weathertwin/WeathertwinApplication.java
+++ src/main/java/app/weathertwin/WeathertwinApplication.java
@@ -1,8 +1,12 @@
 package app.weathertwin;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+
+import app.weathertwin.repository.WeatherDataRepository;
 
 @SpringBootApplication
 @PropertySource("classpath:application-dev.properties")
@@ -10,7 +14,16 @@ public class WeathertwinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeathertwinApplication.class, args);
-		System.out.println("Weather Twin is running");
 	}
 
+	@Bean
+	public CommandLineRunner weatherTwinCLR(WeatherDataRepository weatherDataRepository) {
+		return (args) -> {
+
+			System.out.println("Weather Twin is running");
+
+			// TODO: add test lats and lons here, fetch the weather data with a loop (?),
+			// convert the data with DTO to an entity, save to database
+		};
+	}
 }
