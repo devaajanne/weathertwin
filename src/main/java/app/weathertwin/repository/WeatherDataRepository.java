e3020211abf8500e7dbef51b894bb4363b389c8b
devaajanne <devaajanne@gmail.com>
2025-01-02 09:35:06 +0200

    Adde SQL query to repository to find a similar weather location

--- src/main/java/app/weathertwin/repository/WeatherDataRepository.java
+++ src/main/java/app/weathertwin/repository/WeatherDataRepository.java
@@ -1,9 +1,19 @@
 package app.weathertwin.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import app.weathertwin.entity.WeatherData;
 
 public interface WeatherDataRepository extends CrudRepository<WeatherData, Long> {
 
+    // https://www.baeldung.com/spring-data-jpa-query
+    @Query(value = "SELECT * FROM weatherdatatable WHERE temp BETWEEN :minTemp AND :maxTemp AND id != :id", nativeQuery = true)
+    List<WeatherData> findWeatherDataThatMeetsConditions(
+            @Param("minTemp") Double minTemp,
+            @Param("maxTemp") Double maxTemp,
+            @Param("id") Long id);
 }
