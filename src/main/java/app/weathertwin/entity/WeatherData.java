a1fe4215fa9b843a5c9e9f8a95979c08e5743e5e
devaajanne <devaajanne@gmail.com>
2025-01-02 09:38:11 +0200

    Turned the WeatherData entity into a database table, so SQL queries work

--- src/main/java/app/weathertwin/entity/WeatherData.java
+++ src/main/java/app/weathertwin/entity/WeatherData.java
@@ -2,8 +2,10 @@ package app.weathertwin.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "weatherdatatable")
 public class WeatherData {
 
     @Id
