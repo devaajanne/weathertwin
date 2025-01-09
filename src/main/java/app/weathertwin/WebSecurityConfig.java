751d6ff43daee3a14b70ea9a363d020190cb00ae
devaajanne <devaajanne@gmail.com>
2025-01-09 14:50:20 +0200

    Added web security config

---
+++ src/main/java/app/weathertwin/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package app.weathertwin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final CorsConfig corsConfig;
+
+    public WebSecurityConfig(CorsConfig corsConfig) {
+        this.corsConfig = corsConfig;
+    }
+
+    @Bean
+    public SecurityFilterChain configureSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors(cors -> cors.configurationSource(corsConfig.getCorsConfigurationSource()));
+
+        return httpSecurity.build();
+    }
+}
