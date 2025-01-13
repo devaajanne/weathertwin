21dd04c53ebbd8012a7f08a7f5e97cde13d8e04d
devaajanne <devaajanne@gmail.com>
2025-01-13 17:17:04 +0200

    Removed unused endpoints and imports

--- src/main/java/app/weathertwin/controller/WeathertwinController.java
+++ src/main/java/app/weathertwin/controller/WeathertwinController.java
@@ -1,26 +1,17 @@
 package app.weathertwin.controller;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import app.weathertwin.entity.WeatherData;
-import app.weathertwin.repository.WeatherDataRepository;
 import app.weathertwin.service.HttpService;
 import app.weathertwin.service.ConversionService;
 import app.weathertwin.service.QueryService;
@@ -29,11 +20,9 @@ import app.weathertwin.service.QueryService;
 @RequestMapping("api")
 public class WeathertwinController {
 
-    private final WeatherDataRepository weatherDataRepository;
     private final QueryService queryService;
 
-    public WeathertwinController(WeatherDataRepository weatherDataRepository, QueryService queryService) {
-        this.weatherDataRepository = weatherDataRepository;
+    public WeathertwinController(QueryService queryService) {
         this.queryService = queryService;
     }
 
@@ -64,91 +53,5 @@ public class WeathertwinController {
         }
 
         return returnedMap;
-    }
-
-    private static String API_KEY;
-
-    @Value("${APIkey}")
-    public void setStaticName(String name) {
-        API_KEY = name;
-    }
-
-    // Endpoint for testing returned geocoding data as json
-    // Not meant for production
-    @GetMapping("/geocoding")
-    public JsonNode getCityGeocoding(@RequestBody JsonNode requestBody) {
-        JsonNode rootNode = null;
-
-        String city = requestBody.get("city").asText();
-
-        final String GEOCODING_URL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid="
-                + API_KEY;
-
-        // https://www.baeldung.com/java-9-http-client
-        // https://www.baeldung.com/java-uri-create-and-new-uri
-        HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create(GEOCODING_URL))
-                .GET()
-                .build();
-
-        try {
-            HttpResponse<String> response = HttpClient.newHttpClient().send(httpRequest,
-                    HttpResponse.BodyHandlers.ofString());
-
-            // https://www.baeldung.com/jackson-object-mapper-tutorial
-            ObjectMapper objectMapper = new ObjectMapper();
-            rootNode = objectMapper.readTree(response.body());
-
-        } catch (IOException | InterruptedException exception) {
-            exception.printStackTrace();
-        }
-
-        return rootNode;
-    }
-
-    // Endpoint for testing returned weather data as json
-    // Not meant for production
-    @GetMapping("/weatherdatajson")
-    public JsonNode getWeatherDataJson(@RequestBody JsonNode requestBody) {
-        JsonNode rootNode = null;
-
-        String lat = requestBody.get("lat").asText();
-        String lon = requestBody.get("lon").asText();
-
-        final String WEATHERDATA_URL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon
-                + "&appid=" + API_KEY;
-
-        // https://www.baeldung.com/java-9-http-client
-        // https://www.baeldung.com/java-uri-create-and-new-uri
-        HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create(WEATHERDATA_URL))
-                .GET()
-                .build();
-
-        try {
-            HttpResponse<String> response = HttpClient.newHttpClient().send(httpRequest,
-                    HttpResponse.BodyHandlers.ofString());
-
-            // https://www.baeldung.com/jackson-object-mapper-tutorial
-            ObjectMapper objectMapper = new ObjectMapper();
-            rootNode = objectMapper.readTree(response.body());
-
-        } catch (IOException | InterruptedException exception) {
-            exception.printStackTrace();
-        }
-
-        return rootNode;
-    }
-
-    @GetMapping("/findbyid")
-    public WeatherData findById(@RequestBody JsonNode requestBody) {
-        Long id = requestBody.get("id").asLong();
-
-        return (weatherDataRepository.findById(id).get());
-    }
-
-    @GetMapping("/findall")
-    public Iterable<WeatherData> findAll() {
-        return (weatherDataRepository.findAll());
     }
 }
