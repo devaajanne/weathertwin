fe0f41511cf7345b67778e73320e5964dafe93bf
devaajanne <devaajanne@gmail.com>
2025-02-16 13:11:10 +0200

    Removed @PropertySource

--- src/main/java/app/weathertwin/WeathertwinApplication.java
+++ src/main/java/app/weathertwin/WeathertwinApplication.java
@@ -2,11 +2,9 @@ package app.weathertwin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@PropertySource("classpath:application-dev.properties")
 @EnableScheduling
 public class WeathertwinApplication {
 
