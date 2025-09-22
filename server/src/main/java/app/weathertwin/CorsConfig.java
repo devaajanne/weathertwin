a8ee3911a1e34d184b9b9f4e1afc3d9b547ebe9b
devaajanne <devaajanne@gmail.com>
2025-09-22 19:01:23 +0300

    issue/#9 Make CORS allowed origins dynamic * CORS allowed origins are now loaded from application-XXX.properties files based on environment

--- server/src/main/java/app/weathertwin/CorsConfig.java
+++ server/src/main/java/app/weathertwin/CorsConfig.java
@@ -1,6 +1,7 @@
 package app.weathertwin;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,6 +12,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
+  /*
+   * CORS allowed origins is referenced from application properties file.
+   */
+  private static String CORS_ALLOWED_ORIGIN;
+
+  @Value("${CorsAllowedOrigin}")
+  public void setStaticName(String origin) {
+    CORS_ALLOWED_ORIGIN = origin;
+  }
+
   /**
    * Here we set the configuration for cross-origin resource sharing so that our front-end client
    * can send requests to our back-end server.
@@ -20,7 +31,7 @@ public class CorsConfig {
   @Bean
   public CorsConfigurationSource getCorsConfigurationSource() {
     CorsConfiguration corsConfig = new CorsConfiguration();
-    corsConfig.setAllowedOrigins(List.of("https://devaajanne.github.io/weathertwin/"));
+    corsConfig.setAllowedOrigins(List.of(CORS_ALLOWED_ORIGIN));
     corsConfig.setAllowedMethods(List.of("POST", "OPTIONS")); // OPTIONS is needed due to pre-flight
     corsConfig.setAllowedHeaders(List.of("Content-Type"));
 
